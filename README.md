119b4816251113e83b2d0505962a5347251dbb6e
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-10-31 21:32:21 -0500

    Updated ReadMe and added testing script

--- README.md
+++ README.md
@@ -1,12 +1,38 @@
 # Team-Git-Good-CSCI3501-Sorting-Competition
 
 ## Data Generation
-### Prompts for Output files:
+
+### Prompts for Output files
+
 `java DataGenerator Outputfile number-of-lines rate-parameter`
-#### For Outputs Files 1-6:
+
+#### For Outputs Files 1-6
+
 * Output1.txt 100000 .1
 * Output2.txt 100000 .001
 * Output3.txt 500000 .1
 * Output4.txt 500000 .001
 * Output5.txt 1000000 .1
 * Output6.txt 1000000 .001
+
+## Testing the sorting algorithm
+
+* Compile the sorting competition file using `javac sortingfile`
+* Run the compiled code using `java sortingfile input-filename output-filename`
+
+### Group0 as Example
+
+#### Compile the Java File
+
+`javac Group0`
+
+#### Run the file
+
+`java Group0 output.txt sortedoutput.txt`
+
+## Automatic Testing
+
+The repository includes a script for testing a sort and whether or not it sorted correctly!
+
+In order to use it use
+`./testSort.sh test-number sortingfile`
--- testSort.sh
+++ testSort.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-#Assumes all files are compiled
+# Assumes all files are compiled and data is prepared
 test() {
     local testNumber="$1" #first input is number of Output(n).txt
     local testSort="$2" #second input is the compiled sort algorithm you want to test
     local inputfile="Output${testNumber}.txt"
+    local sortedoutput="Sorted${testNumber}.txt"
+    local correctoutput="Sorted${testNumber}Correct.txt"
+    local test="test.txt"
+    touch "${sortedoutput}"
+    touch "${correctoutput}"
 
     echo "input file: ${inputfile}"
+    echo "${test}"
+
+    # Run Group0 for correct Output
+    echo "Group 0"
+    java Group0 "${inputfile}" "${correctoutput}" # >> Group0Time.txt
+
+    # Run input sort
+    echo "$2"
+    java "$2" "${inputfile}" "${sortedoutput}" # >> TestedSortTime.txt
+
+
+    # diff ${sortedoutput} ${correctoutput}  >> diff.txt
+    $(diff ${sortedoutput} ${correctoutput})
+        if [ $? -eq 0 ]
+            then 
+                echo "Files are equal!"
+            else
+                echo "Files are different!"
+            fi
 }
 test "$@"
